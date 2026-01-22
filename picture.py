c280f0970002c58e98e62cbfded02cc5f0c25f71
Starbirst934 <nova.james.warner@icloud.com>
2026-01-22 15:27:43 -0500

    Add files via upload

---
+++ picture.py
@@ -0,0 +1,31 @@
+"""
+Two Family members a granny and a dollar kid,the two people outside, live in the house below (building on the left).
+On the right is their gargage with their convertible car.
+Next to the house is a big bird on the ground taking a break from flying. 
+The convertible's license plate is named "HI".
+The path infront of the house leads to the door.
+Made by Nova James Warner
+"""
+print("|\============================\                          /^^^^^^^^^^^^^^^^^^^^^^^\                   ")
+print("| \ ===========================\                        /                         \                  ")
+print("|  \ ===========================\                      /                           \                 ")
+print("|   \ ===========================\                    /                             \                ")
+print("|    ------------------------------                  |\                              \               ")
+print("|    |     ______       ______    |                  |  \     ________________________\              ")
+print("|    |    |      |     |      |   |                  |   \   |  _____________________  |             ")
+print("|    |    |      |     |      |   |                  |    \  | |                     | |             ")
+print("|    |    |      |     |      |   |                  |     \ | |                     | |             ")
+print("|    |    |______|     |______|   |                   \     \| |    ____________     | |             ") 
+print("|    |                            |     ,,,,,,     ???????   | |   |____________|    | |             ")
+print("\    |            _____           |    (0  v 0)   ($  x  $)  | |  /              \   | |             ")
+print(" \   |           |     |          |     oooooo     ~~~~~~~   | | |--   ______   --|  | |             ")
+print("  \  |           |     |          |    /|____|\   /|_____|\  | | |____|__HI__|____|  | |             ")
+print("   \ |           |    @|          |     |    |     |     | \ | |  |__|        |__|   | |             ")
+print("    \|___________|_____|__________|     !    !     !     !  \|_|_____________________|_|             ")
+print("                 ::::::                                                                              ")
+print("      _            ::::::                                                                            ")
+print("    < 0 )           :::::::                                                                          ")
+print("    (   )            :::::::                                                                         ")
+print("     - -          :::::::                                                                            ")
+print("                :::::::                                                                              ")
+print("               :::::::                                                                               ")
